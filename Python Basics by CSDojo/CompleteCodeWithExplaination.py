bc554d608021634bbc8cc87c9c183926167d3e98
Prakhar Kumar Chandraker <prakharkumarchandraker@gmail.com>
2023-07-09 15:44:38 +0530

    Update CompleteCodeWithExplaination.py

--- Python Basics by CSDojo/CompleteCodeWithExplaination.py
+++ Python Basics by CSDojo/CompleteCodeWithExplaination.py
@@ -50,10 +50,84 @@ else:
 print("This is outside the if else clause")
 
 e = 5
-f = 8
+f = 6
 if e < f:
-    print(" e less than f")
+    print("e less than f")
 elif e == f:  # here == means equal to, = in python means assigning operation
-    print(" e is equal to f")
+    print("e is equal to f")
 else:
     print("nothing")
+
+# we can also have nested if else
+g = 7
+h = 8
+if g < h:
+    print('g is less than h')
+else:
+    if g == h:
+        print("g=h")
+    else:
+        print("nothing")
+
+"""
+Arithmetic operators
+/ means divide
+* means multiply
+** means power
++ means addition
+- means subtraction
+% means modulus
+// means floor division 
+ The division operator / in Python returns the exact result of the division operation as a floating-point number
+ , while the floor division operator // returns the quotient rounded down to the nearest integer
+"""
+"""
+Assignment Operators
+= equal
++= example is x+=3 means x=x+3
+-= example is x-=3 means x=x-3
+*= example is x*=3 means x=x*3
+/= 
+//=
+%=
+**=
+&= Performs Bitwise AND on operands and assign value to left operand	a &= b   
+|= Performs Bitwise OR on operands and assign value to left operand	a |= b    
+^= Performs Bitwise xOR on operands and assign value to left operand	a ^= b    
+>>= Performs Bitwise right shift on operands and assign value to left operand	a >>= b     
+<<= Performs Bitwise left shift on operands and assign value to left operand	a <<= b
+"""
+"""
+Identity Operators
+is  True if the operands are identical 
+is not True if the operands are not identical 
+"""
+a = 10
+b = 20
+c = a
+
+print(a is not b)
+print(a is c)
+
+"""
+Membership Operators
+in            True if value is found in the sequence
+not in        True if value is not found in the sequence
+"""
+# Python program to illustrate
+# not 'in' operator
+x = 24
+y = 20
+list = [10, 20, 30, 40, 50]
+
+if x not in list:
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if y in list:
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
+
+
